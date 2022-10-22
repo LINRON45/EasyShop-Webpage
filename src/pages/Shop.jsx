37bfa272323befb1b0fc00dcf5b7a7e46850c5de
LINRON45 <linronj@gmail.com>
2022-10-22 19:12:11 -0400

    updated

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,20 +3,24 @@ import ItemBox from "../components/Shop/Item-box";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../services/firebase-config";
 import Zoom from "@mui/material/Zoom";
-import { isEmpty } from "@firebase/util";
+import LoadingOverlay from "react-loading-overlay-ts";
+import { GridLoader } from "react-spinners";
 
 function Shop() {
   const [itemsArr, setItemsArr] = useState([]);
-  const [pageText, setText] = useState("Loading...");
+  const [pageText, setText] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getData() {
+      setLoading(true);
       const users = await getDocs(collection(db, "Users"));
-
 
       users.forEach(async (user) => {
         const items = await getDocs(collection(db, `Users/${user.id}/Sales`));
         if (items.empty) {
+          setLoading(false);
+
           setText("No Products On Sale!");
         }
 
@@ -24,6 +28,8 @@ function Shop() {
           setItemsArr((prev) => [...prev, item.data()]);
         });
       });
+
+      return setLoading(false);
     }
 
     getData();
@@ -34,17 +40,23 @@ function Shop() {
   }
 
   return (
-    <Zoom in={true}>
-      <div className="Shop">
-        <div className="list-items">
-          {itemsArr instanceof Array && itemsArr.length ? (
-            itemsArr.map((item, index) => createItembox(item, index))
-          ) : (
-            <p>{pageText}</p>
-          )}
+    <LoadingOverlay
+      id="loading-Overlay"
+      active={loading}
+      spinner={<GridLoader color="#ff5c33" />}
+    >
+      <Zoom in={true}>
+        <div className="Shop">
+          <div className="list-items">
+            {itemsArr instanceof Array && itemsArr.length ? (
+              itemsArr.map((item, index) => createItembox(item, index))
+            ) : (
+              <p>{pageText}</p>
+            )}
+          </div>
         </div>
-      </div>
-    </Zoom>
+      </Zoom>
+    </LoadingOverlay>
   );
 }
 
